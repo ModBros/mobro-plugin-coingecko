3a361f042bca1cb3adc7f3e8444166726f7c1a5e
Manfred Graf <manfred.graf91@gmail.com>
2023-06-06 11:58:28 +0200

    feat: created project based on template

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MoBro.Plugin.SDK.Testing;
+using MoBro.Plugin.CoinGecko;
+
+// create and start the plugin to test it locally
+var plugin = MoBroPluginBuilder
+  .Create<Plugin>()
+  .Build();
+
+Console.ReadLine();
