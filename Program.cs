351c97935e6dd2e995e4eaf0e84c8c6fdc6e4bd9
Manfred Graf <manfred.graf91@gmail.com>
2025-06-09 22:52:12 +0200

    feat: update SDK + update to net8.0

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using MoBro.Plugin.CoinGecko;
 using MoBro.Plugin.SDK;
 
 // create and start the plugin to test it locally
-var plugin = MoBroPluginBuilder
+using var plugin = MoBroPluginBuilder
   .Create<Plugin>()
   .WithSetting(Ids.SettingUpdateFrequency, "1")
   .WithSetting(Ids.SettingCurrency, "usd")
